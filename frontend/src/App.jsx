5a296e7d4835109aee5a9bce1aca50ac34cf2ba5
Bahar Abbasi Delvand <baharcantfly@gmail.com>
2024-05-21 17:41:37 -0400

    made the spinner component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Home } from "./pages/Home";
+import { CreateBooks } from "./pages/CreateBooks";
+import { DeleteBooks } from "./pages/DeleteBooks";
+import { EditBooks } from "./pages/EditBooks";
+import { ShowBooks } from "./pages/ShowBooks";
 
+import {Routes, Route} from 'react-router-dom';
 const App =() => {
   return(
-    <div>
-
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<ShowBooks/>} />
+      <Route path='/books/edit/:id' element={<EditBooks/>} />
+      <Route path='/books/delete/:id' element={<DeleteBooks/>} />
+    </Routes>
   )
 }
 export default App;
